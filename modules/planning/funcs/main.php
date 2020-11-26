62b7d2afcee3373fa3030ab3800ca1a00bcff4fe
nhoxbin <khoaij123@gmail.com>
2020-11-26 14:14:06 +0700

    prepare module planning

---
+++ modules/planning/funcs/main.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Bin (JBN)
+ * @Copyright (C) 2020 BinPC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Sun, 15 Nov 2020
+ */
+
+if (!defined('NV_IS_MOD_CALLME')) die('Stop!!!');
+
+nv_redirect_location(NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA);
---
+++ modules/planning/language/admin_vi.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Bin (JBN)
+ * @Copyright (C) 2020 BinPC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Sun, 15 Nov 2020
+ */
+
+if (!defined('NV_ADMIN') or !defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'JBN';
+$lang_translator['createdate'] = '20/09/2012, 04:05';
+$lang_translator['copyright'] = '@Copyright (C) 2012 VINADES.,JSC. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
